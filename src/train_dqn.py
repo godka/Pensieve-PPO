ba8f356088168014c78b8e6d5b7b94b58129287b
godka <htc17@mails.tsinghua.edu.cn>
2020-08-02 20:02:25 +0800

    edit

--- src/train_dqn.py
+++ src/train_dqn.py
@@ -161,13 +161,17 @@ def agent(agent_id, net_params_queue, exp_queue):
 
                 action_prob = actor.predict(
                     np.reshape(obs, (1, S_DIM[0], S_DIM[1])))
-
+                
+                if np.random.uniform() < 0.2:
+                    bit_rate = np.random.randint(A_DIM)
+                else:
+                    bit_rate = np.argmax(action_prob)
                 #action_cumsum = np.cumsum(action_prob)
                 #bit_rate = (action_cumsum > np.random.randint(
                 #    1, RAND_RANGE) / float(RAND_RANGE)).argmax()
                 # gumbel noise
-                noise = np.random.gumbel(size=len(action_prob))
-                bit_rate = np.argmax(np.log(action_prob) + noise)
+                # noise = np.random.gumbel(size=len(action_prob))
+                # bit_rate = np.argmax(np.log(action_prob) + noise)
 
                 obs, rew, done, info = env.step(bit_rate)
 
