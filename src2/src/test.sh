c4ce63f4a2687a9ae1ccde709085d5cc9dda495f
donglinb <baidonglin295332@gmail.com>
2022-09-20 20:16:17 +0000

    update

---
+++ src2/src/test.sh
@@ -0,0 +1,8 @@
+
+python test2.py --trace=noaa
+python test2.py --trace=london
+python test2.py --trace=shanghai
+python test2.py --trace=new_york
+python test2.py --trace=sydney
+python test2.py --trace=HK
+python test2.py --trace=sanfan
