cc832e1d327bd6a592b535d815095ddffe057cea
KyoungjunPark <parkkjun525@gmail.com>
2022-10-23 14:58:36 -0500

    [Update] bandwidth sharing RL models.

--- src/train_bw_share.py
+++ src/train_bw_share.py
@@ -22,7 +22,7 @@ RANDOM_SEED = 42
 SUMMARY_DIR = './ppo'
 MODEL_DIR = './models'
 TRAIN_TRACES = './train/'
-TEST_LOG_FOLDER = './test_results/'
+TEST_LOG_FOLDER = './test_results'
 LOG_FILE = SUMMARY_DIR + '/log'
 PPO_TRAINING_EPO = 5
 
