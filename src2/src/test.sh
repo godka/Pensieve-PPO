0356469defa79d3ce18ce92a231a736a502c5254
KyoungjunPark <parkkjun525@gmail.com>
2022-10-23 12:36:28 -0500

    [Update] bandwidth sharing RL models.

--- src2/src/test.sh
+++
@@ -1,8 +0,0 @@
-
-python test2.py --trace=noaa
-python test2.py --trace=london
-python test2.py --trace=shanghai
-python test2.py --trace=new_york
-python test2.py --trace=sydney
-python test2.py --trace=HK
-python test2.py --trace=sanfan
