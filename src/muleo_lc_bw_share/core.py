e0ecd2cd255010ed8359f8c7ba62e61a6e8221c3
KyoungjunPark <parkkjun525@gmail.com>
2022-11-04 13:46:52 -0500

    [Update] Explicit RL.

--- src/muleo_lc_bw_share/core.py
+++ src/muleo_lc_bw_share/core.py
@@ -102,8 +102,7 @@ class Environment:
         """
         if id_list is None:
             sat_id = self.next_sat_id[agent]
-        print(self.mahimahi_ptr[agent])
-        print(sat_id)
+
         self.connection[sat_id][self.mahimahi_ptr[agent]] = agent
 
         if sat_id == self.cur_sat_id[agent]:
