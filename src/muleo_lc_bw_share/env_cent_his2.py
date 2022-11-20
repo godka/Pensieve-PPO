f2113755add622b4e5ee34700d67a8bf74263fff
KJ Park <kjpark@cs.utexas.edu>
2022-11-19 20:37:54 -0600

    [Updates] Normalized the data

--- src/muleo_lc_bw_share/env_cent_his2.py
+++ src/muleo_lc_bw_share/env_cent_his2.py
@@ -117,8 +117,7 @@ class ABREnv():
         encoded_others_last_bit_rate = one_hot_encode(others_last_bit_rate, len(VIDEO_BIT_RATE))
 
         state[(12 + MAX_SAT - A_SAT + PAST_SAT_LOG_LEN + (self.num_agents - 1) * PAST_SAT_LOG_LEN):
-                     (12 + MAX_SAT - A_SAT + PAST_SAT_LOG_LEN + (self.num_agents - 1) * PAST_SAT_LOG_LEN + (
-                                 self.num_agents - 1)),
+                     (12 + MAX_SAT - A_SAT + PAST_SAT_LOG_LEN + (self.num_agents - 1) * PAST_SAT_LOG_LEN + self.num_agents - 1),
         0:len(VIDEO_BIT_RATE)] = encoded_others_last_bit_rate
 
         self.state[agent] = state
@@ -247,9 +246,9 @@ class ABREnv():
 
         others_last_bit_rate = self.last_bit_rate[:agent-1] + self.last_bit_rate[agent:]
         state[(12 + MAX_SAT - A_SAT + PAST_SAT_LOG_LEN + (self.num_agents - 1) * PAST_SAT_LOG_LEN):
-                     (12 + MAX_SAT - A_SAT + PAST_SAT_LOG_LEN + (self.num_agents - 1) * PAST_SAT_LOG_LEN + (
-                                 self.num_agents - 1) * len(VIDEO_BIT_RATE)),
-        0:len(VIDEO_BIT_RATE)] = np.reshape(one_hot_encode(others_last_bit_rate, len(VIDEO_BIT_RATE)), (-1, 5))
+                     (12 + MAX_SAT - A_SAT + PAST_SAT_LOG_LEN + (self.num_agents - 1) * PAST_SAT_LOG_LEN +
+                                 self.num_agents - 1),
+        0:len(VIDEO_BIT_RATE)] = np.reshape(one_hot_encode(others_last_bit_rate, len(VIDEO_BIT_RATE)), (-1, len(VIDEO_BIT_RATE)))
         # if len(next_sat_user_nums) < PAST_LEN:
         #     next_sat_user_nums = [0] * (PAST_LEN - len(next_sat_user_nums)) + next_sat_user_nums
 
