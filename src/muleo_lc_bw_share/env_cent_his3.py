6de50143d6ba5a2a1c58dd114e5da727df8ba541
KJ Park <kjpark@cs.utexas.edu>
2022-11-19 23:45:50 -0600

    New Feature

--- src/muleo_lc_bw_share/env_cent_his3.py
+++ src/muleo_lc_bw_share/env_cent_his3.py
@@ -175,7 +175,7 @@ class ABREnv():
             self.is_handover = True
         else:
             print("Never!")
-        self.prev_sat_id, self.cur_sat_id = net_env.set_satellite(agent, sat[agent])
+        self.prev_sat_id, self.cur_sat_id = self.net_env.set_satellite(agent, sat[agent])
 
     def step(self, action, agent):
         bit_rate = int(action) % A_DIM
