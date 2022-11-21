89af33ed849f25f9965615a796c9a728c2cc6d3a
KyoungjunPark <parkkjun525@gmail.com>
2022-11-20 22:45:58 -0600

    [Update] Cent RL for 6 users.

--- src/train_cent_his2.py
+++ src/train_cent_his2.py
@@ -22,10 +22,10 @@ TRAIN_SEQ_LEN = 1000  # take as a train batch
 TRAIN_EPOCH = 500000
 MODEL_SAVE_INTERVAL = 100
 RANDOM_SEED = 42
-SUMMARY_DIR = './ppo_cent_his2'
+SUMMARY_DIR = './ppo_cent_his22'
 MODEL_DIR = './models'
 TRAIN_TRACES = './train/'
-TEST_LOG_FOLDER = './test_results_cent_his2'
+TEST_LOG_FOLDER = './test_results_cent_his_22'
 PPO_TRAINING_EPO = 5
 
 import argparse
