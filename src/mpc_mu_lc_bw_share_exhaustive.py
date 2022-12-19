b87a17398863d51e0cc73b57d0b41d0be4f1bab5
KJ Park <kjpark@cs.utexas.edu>
2022-12-19 16:13:51 -0600

    [New Feature] User class & fixed SNR

--- src/mpc_mu_lc_bw_share_exhaustive.py
+++ src/mpc_mu_lc_bw_share_exhaustive.py
@@ -22,7 +22,7 @@ BITRATE_REWARD = [1, 2, 3, 12, 15, 20]
 TOTAL_VIDEO_CHUNKS = 48
 RANDOM_SEED = 42
 RAND_RANGE = 1000000
-SUMMARY_DIR = 'test_results_exhaustive_ratio/'
+SUMMARY_DIR = 'test_results_reduced_ratio/'
 LOG_FILE = SUMMARY_DIR + 'log_sim_cent'
 TEST_TRACES = './test_tight/'
 SUMMARY_PATH = SUMMARY_DIR + 'summary'
@@ -73,7 +73,7 @@ size_video6 = [181801, 155580, 139857, 155432, 163442, 126289, 153295, 173849, 1
 
 MPC_TYPE = "DualMPC"
 MPC_TYPE = "DualMPC-Centralization-Exhaustive"
-# MPC_TYPE = "DualMPC-Centralization-Reduced"
+MPC_TYPE = "DualMPC-Centralization-Reduced"
 
 # DualMPC-Centralization
 
