3ec42e3ce36fd2bbbc93759cab0869959f01a518
KyoungjunPark <parkkjun525@gmail.com>
2023-02-17 21:42:32 -0600

    [Refactor] Update the Pensieve

--- src/models/rl_multi_bw_share/train_pensieve_time.py
+++ src/models/rl_multi_bw_share/train_pensieve_time.py
@@ -24,7 +24,6 @@ MODEL_SAVE_INTERVAL = 3000
 RANDOM_SEED = 42
 SUMMARY_DIR = './pensieve'
 MODEL_DIR = '..'
-TRAIN_TRACES = 'data/sat_data/train/'
 TEST_LOG_FOLDER = './test_results_pensieve'
 PPO_TRAINING_EPO = 5
 
