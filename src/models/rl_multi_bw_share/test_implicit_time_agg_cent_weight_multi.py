6db11b8ebc8d66a553a49d4090c91c2721ec6e2b
KyoungjunPark <parkkjun525@gmail.com>
2023-03-03 01:46:40 -0600

    [Update] Multi models.

--- src/models/rl_multi_bw_share/test_implicit_time_agg_cent_weight_multi.py
+++ src/models/rl_multi_bw_share/test_implicit_time_agg_cent_weight_multi.py
@@ -4,7 +4,7 @@ root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, root_dir + '/../')
 from util.constants import CHUNK_TIL_VIDEO_END_CAP, BUFFER_NORM_FACTOR, VIDEO_BIT_RATE, REBUF_PENALTY, SMOOTH_PENALTY, \
     DEFAULT_QUALITY, BITRATE_WEIGHT, M_IN_K, A_DIM, S_LEN, PAST_LEN, BITRATE_REWARD, TEST_TRACES, PAST_SAT_LOG_LEN, A_SAT
-from util.encode import encode_other_sat_info, one_hot_encode
+from util.encode_multi import encode_other_sat_info, one_hot_encode
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 import numpy as np
