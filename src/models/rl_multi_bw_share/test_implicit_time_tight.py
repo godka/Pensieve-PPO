3d0176211da9667c7ea2ca54cfa72946e550142a
KyoungjunPark <parkkjun525@gmail.com>
2023-03-04 21:14:34 -0600

    [Update] Multi models.

--- src/models/rl_multi_bw_share/test_implicit_time_tight.py
+++ src/models/rl_multi_bw_share/test_implicit_time_tight.py
@@ -3,7 +3,7 @@ import sys
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, root_dir + '/../')
 from util.constants import CHUNK_TIL_VIDEO_END_CAP, BUFFER_NORM_FACTOR, VIDEO_BIT_RATE, REBUF_PENALTY, SMOOTH_PENALTY, \
-    DEFAULT_QUALITY, BITRATE_WEIGHT, M_IN_K, A_DIM, S_LEN, PAST_LEN, BITRATE_REWARD, TEST_TRACES
+    DEFAULT_QUALITY, BITRATE_WEIGHT, M_IN_K, A_DIM, S_LEN, PAST_LEN, BITRATE_REWAR, TEST_TIGHT_TRACES
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 import numpy as np
