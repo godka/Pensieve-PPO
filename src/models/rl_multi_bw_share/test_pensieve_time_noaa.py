cdffd8812a379fc77e5f1d215d8546bd149cb427
KyoungjunPark <parkkjun525@gmail.com>
2023-03-09 16:56:12 -0600

    [New Feature] NOAA for mpc.

--- src/models/rl_multi_bw_share/test_pensieve_time_noaa.py
+++ src/models/rl_multi_bw_share/test_pensieve_time_noaa.py
@@ -23,7 +23,8 @@ NN_MODEL = sys.argv[1]
 USERS = int(sys.argv[2])
 HO_TYPE = str(sys.argv[3])
 SUMMARY_DIR = './test_results_pensieve_noaa' + str(USERS)
-
+if not os.path.exists(SUMMARY_DIR):
+    os.makedirs(SUMMARY_DIR)
 LOG_FILE = SUMMARY_DIR + '/log_sim_pensieve_noaa'
 SUMMARY_PATH = SUMMARY_DIR + '/summary'
 
